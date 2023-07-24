33d435918ffc2dde6453f4d72c2692b6aff4007e
Alejandro LV <larreta_vzla@hotmail.com>
2023-07-24 08:12:59 -0700

    Faltaba students

--- src/students/students.module.ts
+++ src/students/students.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { StudentsService } from './students.service';
 import { StudentsController } from './students.controller';
+import { Student } from './entities/student.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Student])],
   controllers: [StudentsController],
-  providers: [StudentsService]
+  providers: [StudentsService],
+  exports: [StudentsService],
 })
 export class StudentsModule {}
