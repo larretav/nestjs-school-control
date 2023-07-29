2872136ba16a8e541872421b4b6ce30c227e2e72
larretav <larreta_vzla@hotmail.com>
2023-07-29 13:00:34 -0700

    Sincronización en el trabajo

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -4,11 +4,13 @@ import { SeedController } from './seed.controller';
 import { UsersModule } from 'src/users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
+import { ProfessionalCareer } from 'src/professional_careers/entities/professional_career.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
-      User
+      User,
+      ProfessionalCareer
     ])
   ],
   controllers: [SeedController],
